32187455a507c754342486fd13c86fd11d4a377a
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-13 17:49:37 +0530

    update and gitignore

--- src/main/CreateAccount.java
+++ src/main/CreateAccount.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import main.conn.JDBCConn;
 
 import java.io.IOException;
 import java.io.PrintWriter;
